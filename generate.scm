16cf2d95c7af97e9036b8c7732b94cf2cc3e8eb5
Saarhelo Joonatan <joonatan.saarhelo.aalto.fi>
2019-09-09 17:18:50 +0300

    Limit abduction when MCP is bent
    When making a fist, fingers cannot be spread out.
    
    Also made functions for building names of joints.

--- generate.scm
+++ generate.scm
@@ -1,27 +1,55 @@
-(use-modules (sxml simple) (ice-9 format))
+(use-modules
+ (sxml simple)
+ (ice-9 format)
+ (srfi srfi-1))
 
 (define (finger-geom len)
   `(geom (@ (type "capsule") (size "0.09") (fromto ,(format #f "0 0 0 0 0 ~a" len)))))
+
+(define (pip-name finger) (string-append finger "-pip"))
+(define (dip-name finger) (string-append finger "-dip"))
+(define (mcp-flex-name finger) (string-append finger "-flex"))
+(define (mcp-aa-name finger) (string-append finger "-aa"))
 
 (define (finger name metacarpal proximal middle distal)
   `(,(finger-geom metacarpal)
     (body
      (@ (pos ,(format #f "0 0 ~a" metacarpal)))
      ,(finger-geom proximal)
-     (joint (@ (type "hinge") (axis "0 1 0") (limited "true") (range "-15 90")))
-     (joint (@ (type "hinge") (axis "1 0 0") (limited "true") (range "-25 30")))
+     (joint (@ (name ,(mcp-flex-name name)) (axis "0 1 0") (limited "true") (range "-15 90")))
+     (joint (@ (name ,(mcp-aa-name name)) (axis "1 0 0") (limited "false") (range "-25 30")))
      (body
       (@ (pos ,(format #f "0 0 ~a" proximal)))
       ,(finger-geom middle)
-      (joint (@ (name ,(string-append name "-pip")) (axis "0 1 0") (limited "true") (range "-5 120")))
+      (joint (@ (name ,(pip-name name)) (axis "0 1 0") (limited "true") (range "-5 120")))
       (body
        (@  (pos ,(format #f "0 0 ~a" middle)))
        ,(finger-geom distal)
-       (joint (@ (name ,(string-append name "-dip")) (axis "0 1 0") (limited "true") (range "-5 90")))
+       (joint (@ (name ,(dip-name name)) (axis "0 1 0") (limited "true") (range "-5 90")))
        )))))
 
-(define (dip-pip name)
-  `(joint (@ (joint1 ,(string-append name "-dip")) (joint2 ,(string-append name "-pip")) (polycoef "0 0.66 0 0 0"))))
+(define (dip-pip finger)
+  `(joint (@ (joint1 ,(dip-name finger)) (joint2 ,(pip-name finger)) (polycoef "0 0.66 0 0 0"))))
+
+;;; Limits the abduction of fingers that are bent.
+;;; Left and right limits are modeled separately.
+;;; The left limits double as right limits for hyperextension and vice versa.
+;;;
+;;; The lengths of fixed tendons are calculated with radians.
+;;; -0.44 and 0.52 are approximations of -25 and 30 degrees.
+(define (flex-aa-relation finger)
+  `((fixed
+     (@ (limited "true") (range "-0.44 0.52"))
+     (joint (@ (joint ,(mcp-aa-name finger)) (coef "1")))
+     (joint (@ (joint ,(mcp-flex-name finger)) (coef "0.33"))) ; 0.52 / (pi / 2)
+     )
+    (fixed
+     (@ (limited "true") (range "-0.44 0.52"))
+     (joint (@ (joint ,(mcp-aa-name finger)) (coef "1")))
+     (joint (@ (joint ,(mcp-flex-name finger)) (coef "-0.28"))) ; -0.44 / (pi / 2)
+     )))
+
+(define finger-names '("index" "middle" "ring" "pinky"))
 
 (sxml->xml
  `(mujoco
@@ -49,5 +77,7 @@
        (geom (@ (type "capsule") (size "0.1") (fromto "0 0 0 0 0 0.2")))
        (joint (@ (axis "0 1 0") (limited "true") (range "-20 90")))))))
 
-   (equality ,@(map dip-pip '("index" "middle" "ring" "pinky"))
-    )))
+   (equality ,@(map dip-pip finger-names))
+
+   (tendon ,@(append-map flex-aa-relation finger-names))
+   ))
